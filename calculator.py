abf307fddfd8a1c66cfe47c8aa28be347958ab58
derctyvbun <prudimak12@gmail.com>
2023-02-15 11:58:24 +0300

    calculator project

---
+++ calculator.py
@@ -0,0 +1,32 @@
+# this is a calculator project
+num1=eval(input("Enter no.1 : "))
+num2=eval(input("Enter no.2 : "))
+operator=input("Enter operator : ")
+def add(num1, num2):
+    result= num1 + num2
+    print(result)
+
+def subtract(num1, num2):
+    result= num1 + num2
+    print(result)
+
+def divide(num1, num2):
+    result= num1/num2
+    print(result)
+
+def multiply(num1, num2):
+    result= num1*num2
+    print(result)
+
+if operator == "+":
+    add(num1, num2)
+elif operator == "-":
+    subtract(num1, num2)
+elif operator == "/":
+    divide(num1, num2)
+elif operator == "X" or operator == "x" or operator == "*":
+    multiply(num1, num2)
+else:
+    print("Invalid operator")
+
+
